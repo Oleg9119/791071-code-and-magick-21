fdb51b6c02564b61f5b1158e8ec70de97e5b5cdf
Олег Котляров <o.kotlyarov@r-service.net>
2020-09-26 12:39:03 +0300

    правки кода

--- js/stat.js
+++ js/stat.js
@@ -1,36 +1,50 @@
 'use strict';
 
-const CLOUD_WIDTH = 420;
-const CLOUD_HEIGHT = 270;
-const CLOUD_X = 100;
-const CLOUD_Y = 10;
-const CLOUD_COLOR = '#ffffff';
-const CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
-
-const GAP = 10;
-
-const TITLE_X = CLOUD_X + GAP * 2;
-const TITLE_Y = CLOUD_Y + GAP * 3;
-const SUBTITLE_Y = CLOUD_Y + GAP * 5;
-
-const TEXT_HEIGHT = 16;
-
-const BAR_WIDTH = 40;
-const BAR_GAP = 50;
-const BAR_MAX_HEIGHT = 150;
-const BAR_Y = SUBTITLE_Y + GAP * 2 + TEXT_HEIGHT + BAR_MAX_HEIGHT;
-const BAR_HEIGHT = CLOUD_HEIGHT - SUBTITLE_Y - GAP * 3 - TEXT_HEIGHT * 2;
-
-const MY_BAR_COLOR = 'rgba(255, 0, 0, 1)';
-
-const NAME_Y = 270;
-
-const renderCloud = function (ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+const Cloud = {
+  WIDTH: 420,
+  HEIGHT: 270,
+  X: 100,
+  Y: 10,
+  COLOR: `#ffffff`,
+  SHADOW_COLOR: `rgba(0, 0, 0, 0.7)`,
+  GAP: 10
 };
 
-const getMaxElement = function (arr) {
+const CONTENT_GAP = 25;
+const TEXT_GAP = 5;
+
+const Font = {
+  HEIGHT: 16,
+  COLOR: `#000000`,
+  FONT_FAMILY: 'PT Mono'
+};
+
+const TEXT_X = Cloud.X + CONTENT_GAP;
+const TITLE_Y = Cloud.Y + CONTENT_GAP;
+const SUBTITLE_Y = Cloud.Y + CONTENT_GAP + Font.HEIGHT + TEXT_GAP;
+
+const Bar = {
+  WIDTH: 40,
+  GAP: 50,
+  MAX_HEIGHT: 150,
+  Y: Cloud.Y + Cloud.HEIGHT - (CONTENT_GAP + Font.HEIGHT + TEXT_GAP),
+  MY_BAR_COLOR: `rgba(255, 0, 0, 1)`
+};
+
+const NAME_Y = Cloud.Y + Cloud.HEIGHT - CONTENT_GAP;
+
+const renderCloud = (ctx, x, y, color) => {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, Cloud.WIDTH, Cloud.HEIGHT);
+};
+
+const renderText = (ctx, x, y, text) => {
+  ctx.fillStyle = Font.COLOR;
+  ctx.font = `${Font.HEIGHT}px ${Font.FONT_FAMILY}`;
+  ctx.fillText(text, x, y);
+};
+
+const getMaxElement = (arr) => {
   let maxElement = arr[0];
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
@@ -40,55 +54,44 @@ const getMaxElement = function (arr) {
   return maxElement;
 };
 
-const getRandomValue = function (min, max) {
+const getRandomValue = (min, max) => {
   return min + Math.random() * (max - min);
 };
 
-const getRandomBlueSaturation = function () {
+const getRandomBlueSaturation = () => {
   const randomSaturation = getRandomValue(0, 100);
-  return 'hsl(230, ' + randomSaturation + '%, 60%)';
+  return `hsl(230, ${randomSaturation}%, 60%)`;
 };
 
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_SHADOW_COLOR);
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
+window.renderStatistics = (ctx, names, times) => {
+  renderCloud(ctx, Cloud.X + Cloud.GAP, Cloud.Y + Cloud.GAP, Cloud.SHADOW_COLOR);
+  renderCloud(ctx, Cloud.X, Cloud.Y, Cloud.COLOR);
 
-  ctx.fillStyle = '#000000';
-  ctx.font = '16px "PT Mono"';
-  ctx.fillText('Ура вы победили!', TITLE_X, TITLE_Y);
-  ctx.fillText('Список результатов:', TITLE_X, SUBTITLE_Y);
+  renderText(ctx, TEXT_X, TITLE_Y, `Ура вы победили!`);
+  renderText(ctx, TEXT_X, SUBTITLE_Y, `Список результатов:`);
 
   const maxTime = getMaxElement(times);
 
   for (let i = 0; i < names.length; i++) {
+    const barCount = i + 1;
+    const leftIndent = Cloud.X + Bar.WIDTH * i + Bar.GAP * barCount;
+    const barHeight = -(Bar.MAX_HEIGHT * times[i]) / maxTime;
 
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = MY_BAR_COLOR;
+    if (names[i] === `Вы`) {
+      ctx.fillStyle = Bar.MY_BAR_COLOR;
     } else {
       ctx.fillStyle = getRandomBlueSaturation();
     }
 
     ctx.fillRect(
-      CLOUD_X + BAR_WIDTH * [i + 1] + BAR_GAP * i,
-      BAR_Y,
-      BAR_WIDTH,
-      -(BAR_HEIGHT * times[i]) / maxTime
+      leftIndent,
+      Bar.Y,
+      Bar.WIDTH,
+      barHeight
     );
 
-    ctx.fillStyle = '#000000';
-
-    ctx.fillText(
-      names[i],
-      CLOUD_X + BAR_WIDTH * [i + 1] + BAR_GAP * i,
-      NAME_Y
-    );
-  }
-
-  for (let j = 0; j < times.length; j++) {
-    ctx.fillText(
-      Math.round(times[j]),
-      CLOUD_X + BAR_WIDTH * [j + 1] + BAR_GAP * j,
-      BAR_Y - (BAR_HEIGHT * times[j]) / maxTime - GAP
-    );
+    ctx.fillStyle = Font.COLOR;
+    renderText(ctx, leftIndent, Bar.Y + barHeight - TEXT_GAP, Math.round(times[i]));
+    renderText(ctx, leftIndent, NAME_Y, names[i]);
   }
 };
